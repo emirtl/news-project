a262f906e4c31f90e51e6dbc0b4196eaa944bbc0
hesam <tlemir55@gmail.com>
2025-06-10 04:52:43 +0300

    updated:index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose
     .then(() => console.log('connected to db'))
     .catch((e) => console.log(e));
 
-const PORT = process.env.PORT | 9000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
     console.log('connected to port 9000');
